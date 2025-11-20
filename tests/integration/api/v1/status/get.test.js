70b9a8fda80cb04cbfb88d4ec9c17361c5fae5d1
lpereiras <lpereira.qa@gmail.com>
2025-11-20 17:06:04 -0300

    fix: `.env` -> `.env.development`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,7 @@
 test("GET /api/v1/status should return 200", async () => {
   const res = await fetch("http://localhost:3000/api/v1/status");
+  const resBody = await res.json();
 
   expect(res.status).toBe(200);
+  expect(resBody.status).toBe("OK");
 });
