7b553454084bba92e9f81e369ece8c91417f8e00
lpereiras <lpereira.qa@gmail.com>
2025-11-20 16:38:29 -0300

    add `.env`(learning reasons)&`database.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 5+5 AS result;");
+
+  console.log(result.rows);
   res.status(200).json({ status: "OK" });
 }
 
