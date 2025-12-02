f5c0e7c09ba85aa87efae11ee51488f121c7b085
lpereiras <lpereira.qa@gmail.com>
2025-12-02 10:40:30 -0300

    feat: add useful information to `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,24 @@
 import database from 'infra/database.js';
 
 async function status(req, res) {
-  const result = await database.query('SELECT 5+5 AS result;');
+  const updatedAt = new Date().toISOString();
+  const dbMaxConnections = `SELECT * FROM pg_settings WHERE name = 'max_connections'`;
+  const dbVersion = `SELECT version()`;
+  const dbUsedConnections = `SELECT count(*) FROM pg_stat_activity`;
 
-  console.log(result.rows);
-  res.status(200).json({ status: 'OK' });
+  const maxConnections = await database.query(dbMaxConnections);
+  const version = await database.query(dbVersion);
+  const activeConnections = await database.query(dbUsedConnections);
+
+  res.status(200).json({
+    status: `I'M ALIVE`,
+    updated_at: updatedAt,
+    database: {
+      db_version: version.rows[0].version,
+      db_max_connections: maxConnections.rows[0].setting,
+      db_used_connections: activeConnections.rows[0].count,
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,14 @@
-test('GET /api/v1/status should return 200', async () => {
+test('GET /api/v1/status should return application health status', async () => {
   const res = await fetch('http://localhost:3000/api/v1/status');
   const resBody = await res.json();
+  const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
 
   expect(res.status).toBe(200);
-  expect(resBody.status).toBe('OK');
+  expect(resBody.status).toBe(`I'M ALIVE`);
+  expect(resBody.updated_at).toBeDefined();
+  expect(resBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(resBody.database.db_max_connections).toBeDefined();
+  expect(resBody.database.db_version).toBeDefined();
+  expect(resBody.database.db_used_connections).toBeDefined();
+  expect(resBody.database.db_max_connections).toBe('100');
 });
