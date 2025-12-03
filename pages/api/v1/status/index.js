f8106bc0e41f1990e402f6ac78715494a2fe748f
lpereiras <lpereira.qa@gmail.com>
2025-12-03 10:47:54 -0300

    feat: finish  endpoint & tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,17 +1,26 @@
 import database from 'infra/database.js';
 
 async function status(req, res) {
-  const db_name = 'local_db';
+  const db_name = process.env.POSTGRES_DB;
+  // const db_name = req.query.db_name
+  // that also return the database name
+  // on other hand it will allow external user the
+  // possibility to modify dbOpenConnections query
 
   const updatedAt = new Date().toISOString();
   const dbVersion = await database.query('SHOW server_version;');
   const dbMaxConnections = await database.query('SHOW max_connections;');
-  const dbOpenConnections = await database.query(`SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${db_name}';`);
+  const dbOpenConnections = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [db_name]
+  });
 
   const version = dbVersion.rows[0].server_version;
   const maxConnections = dbMaxConnections.rows[0].max_connections;
   const activeConnections = dbOpenConnections.rows[0].count;
 
+  console.log(activeConnections)
+  
   res.status(200).json({
     status: "I'M ALIVE",
     updated_at: updatedAt,
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test('GET /api/v1/status should return application health status', async () => {
+test('GET /api/v1/status should return application status', async () => {
   const res = await fetch('http://localhost:3000/api/v1/status');
   const resBody = await res.json();
   const parsedUpdatedAt = new Date(resBody.updated_at).toISOString();
@@ -9,7 +9,7 @@ test('GET /api/v1/status should return application health status', async () => {
   expect(resBody.updated_at).toEqual(parsedUpdatedAt);
 
   expect(resBody.database.db_version).toBeDefined();
-  expect(resBody.database.db_version).toBe('16.11')
+  expect(resBody.database.db_version).toBe('16.11');
 
   expect(resBody.database.db_max_connections).toBeDefined();
   expect(resBody.database.db_max_connections).toBe(100);
@@ -18,6 +18,14 @@ test('GET /api/v1/status should return application health status', async () => {
   expect(resBody.database.db_open_connections).toBe(1);
 });
 
-test.skip('GET /api/v1/status SQL injection validation', async () => {
-  await fetch("http://localhost:3000/api/v1/status?db_name='; SELECT pg_sleep(4); --");
+test.skip('GET /api/v1/status SQL injection example', async () => {
+  await fetch(
+    "http://localhost:3000/api/v1/status?db_name='; SELECT pg_sleep(4); --",
+  );
+  // type ? allow send query parameter when application make a request
+  // after =' type ; will finish the query sended to database
+  // and depend on how application use parameterized queries, it could force another query
+  // to execute inside our database, in this fake scenario: SELECT pg_sleep(4).
+  // then you finish the second query with ; and send -- which tell database that everything after
+  // the malicious query is a comment.
 });
